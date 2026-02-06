b591c1f9cbbbd932fdb0f8dc329d0f8b07235a65
David Alejandro Caire Calderon <david.caire@softtekprojects.com>
2026-02-06 11:44:50 -0600

    Make Firebase measurement ID optional

--- scripts/generate-firebase-config.js
+++ scripts/generate-firebase-config.js
@@ -7,8 +7,7 @@ const requiredVars = [
   "FIREBASE_PROJECT_ID",
   "FIREBASE_STORAGE_BUCKET",
   "FIREBASE_MESSAGING_SENDER_ID",
-  "FIREBASE_APP_ID",
-  "FIREBASE_MEASUREMENT_ID"
+  "FIREBASE_APP_ID"
 ];
 
 const missing = requiredVars.filter((name) => !process.env[name]);
@@ -23,9 +22,12 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  appId: process.env.FIREBASE_APP_ID
 };
+
+if (process.env.FIREBASE_MEASUREMENT_ID) {
+  firebaseConfig.measurementId = process.env.FIREBASE_MEASUREMENT_ID;
+}
 
 const output = `// Auto-generated at build time by Netlify\nconst firebaseConfig = ${JSON.stringify(
   firebaseConfig,
@@ -36,4 +38,3 @@ const output = `// Auto-generated at build time by Netlify\nconst firebaseConfig
 const outputPath = path.join(__dirname, "..", "firebase-config.js");
 fs.writeFileSync(outputPath, output, "utf8");
 console.log("Wrote firebase-config.js");
-
